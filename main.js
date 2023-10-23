c4516336777bc98a37f145515daf67ab9e62abf1
niisantanaaa <110932348+niisantanaaa@users.noreply.github.com>
2023-10-23 17:31:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-function tocarTeclaGrilo(){
- document.querySelector('#som_tecla_aplausos').play();
+function tocaAplausos(){
+    document.querySelector('.som_tecla_aplausos').onclick = tocaAplausos;
+}
+function tocaVaia(){
+    document.querySelector('.som_tecla_vaia').onclick = tocaVaia;
+}
+function tocaRisada(){
+    document.querySelector('.som_tecla_risada').onclick = tocaRisada;
+}
+function tocaErrado(){
+    document.querySelector('.som_tecla_errado').onclick = tocaErrado;
+}
+function tocaGrilo(){
+    document.querySelector('.som_tecla_grilo').onclick = tocaGrilo;
+}
+function tocaTambores(){
+    document.querySelector('.som_tecla_tambores').onclick = tocaTambores;
+}
+function tocaTrombetas(){
+    document.querySelector('.som_tecla_trombeta').onclick = tocaTrombetas;
+}
+function tocaDinheiro(){
+    document.querySelector('.som_tecla_dinheiro').onclick = tocaDinheiro;
+}
+function tocaVitoria(){
+    document.querySelector('.som_tecla_vitoria').onclick = tocaVitoria
 }
